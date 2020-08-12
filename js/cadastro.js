959a916a08c30cb05807538556a7ff7183cb704d
CamilaSangon <64562757+CamilaSangon@users.noreply.github.com>
2020-08-12 16:00:38 -0300

    Add files via upload

---
+++ js/cadastro.js
@@ -0,0 +1,45 @@
+$("#btnVoltarHom02").click(function () {
+    window.open('aposentadorias.html','janela');
+});
+
+$("#btnVoltarHom01").click(function () {
+    window.open('aposentadorias.html','janela');
+});
+
+$("#btnVoltarCad01").click(function () {
+    window.open('aposentadorias_novas.html','janela');
+});
+
+$("#btnVoltarCad03").click(function () {
+    window.open('aposentadorias_novas.html','janela');
+});
+
+$("#btnVoltarCad02").click(function () {
+    window.open('aposentadorias_novas.html','janela');
+});
+
+ function MostrarPainelAba(nomeBotao) {
+   if (nomeBotao == 'btnVoltarHom01_1') {
+	//$('#myTabs a[href="##DadosAposentados"]').tab('show');
+  }
+ }
+
+ 
+$("#btnFormulario").click(function () {
+	window.open('formulario1.html','janela');
+});
+
+$('#dvDetalhes').hide();
+$('#btnFechar').hide();
+
+function MostrarEnvios() {
+	$('#dvDetalhes').show();
+	$('#btnFechar').show();
+  }
+$("#btnFechar").click(function () {
+	$('#dvDetalhes').hide();
+	$('#btnFechar').hide();
+});
+
+
+ 
